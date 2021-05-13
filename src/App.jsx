bb29c0704ca125143f22bff055f57521c702c2f2
xueyongfei <pulengba@gmail.com>
2021-05-14 02:27:12 +0800

    feat: webpack基础工程搭建

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div>hello app111</div>
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,5 @@
-import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';
-import Data from './data.xml';
-import printMe from './print';
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App.jsx';
 
-function component() {
-  let element = document.createElement('div');
-  var btn = document.createElement('button');
-
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello')
-
-  // 将图像添加到我们已经存在的 div 中。
-  var myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-  console.log(Data);
-  btn.innerHTML = '点击这f里，然后查看 cffonsole！';
-  btn.onclick = printMe;
-  element.appendChild(btn);
-  return element;
-}
-
-let element = component(); 
-
-document.body.appendChild(element);
-
-if (module.hot) {
-   module.hot.accept('./print.js', function() {
-    document.body.removeChild(element);
-    element = component();
-    document.body.appendChild(element);
-   })
- }
+ReactDom.render(<App />, document.getElementById('app'));
